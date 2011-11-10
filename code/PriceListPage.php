7627bf0417a74458393b1ebceabf03ee35a9e701
Nicolaas <github@sunnysideup.co.nz>
2011-11-10 02:39:23 +0000

    ENHANCEMENT: showing parent pages in price list
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_complex_pricing/trunk@1745 950b781c-c544-efab-2f99-90083ee69683

--- code/PriceListPage.php
+++ code/PriceListPage.php
@@ -2,12 +2,9 @@
 
 /**
  *@author nicolaas[at]
+ *@description
  *
- *
- *
- *
- *
- *
+ * Provides a price list
  *
  *
  *
@@ -18,9 +15,12 @@ class PriceListPage extends ProductGroup {
 
 	public static $icon = "ecommerce_complex_pricing/images/treeicons/PriceListPage";
 
+	public static $db = array(
+		"ShowParentProductGroupPages" => "Int"
+	);
+
 	protected $toHideArray = array(
-		"LevelOfProductsToShow",
-		"ProductsAlsoInOthersGroups"
+		"LevelOfProductsToShow"
 	);
 
 
@@ -31,6 +31,7 @@ class PriceListPage extends ProductGroup {
 		foreach($this->toHideArray as $name) {
 			$fields->removeByName($name);
 		}
+		$fields->addFieldToTab('Root.Content.Products',new CheckboxField("ShowParentProductGroupPages", _t("PriceListPage.NUMBEROFPARENTPAGESTOSHOW", "Show parent group pages with product")));
 		return $fields;
 	}
 
@@ -69,7 +70,30 @@ class PriceListPage extends ProductGroup {
 
 		//ACTION
 		$products = DataObject::get('Product',$where,$sort, null,$limit);
-		$products->TotalCount = $products->count();
+		$allProducts = DataObject::get('Product',$where,$sort);
+		$this->totalCount = $allProducts->count();
+		if($this->ShowParentProductGroupPages) {
+			$dos = null;
+			if($products) {
+				foreach($products as $product) {
+					$dos[$product->ID] = new DataObjectSet();
+					$segmentArray = array();
+					$item = $product;
+					while($item && $item->ParentID) {
+						$item = DataObject::get_by_id("ProductGroup", $item->ParentID);
+						if($item) {
+							$segmentArray[] = array("URLSegment" => $item->URLSegment, "ID" => $item->ID, "ClassName" => $item->ClassName, "Title" => $item->Title, "Link" => $item->Link());
+						}
+					}
+					$segmentArray = array_reverse($segmentArray);
+					foreach($segmentArray as $segment) {
+						$dos[$product->ID]->push(new ArrayData($segment));
+					}
+					$product->ParentSegments = $dos[$product->ID] ;
+					$dos = null;
+				}
+			}
+		}
 		return $products;
 	}
 }
