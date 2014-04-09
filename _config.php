8aed97f74b1d907b7a07d90c0d4bf8178511d66c
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2014-04-09 05:12:56 +0000

    UPGRADE 3.1
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_complex_pricing/trunk@4933 950b781c-c544-efab-2f99-90083ee69683

--- _config.php
+++ _config.php
@@ -1,21 +1,6 @@
 <?php
 
 
-
-//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
-//===================---------------- START ecommerce_group_pricing MODULE ----------------===================
-//MUST SET - WHERE APPLICABLE
-//DataObject::add_extension("Product", "ComplexPriceBuyableDecorator");
-//DataObject::add_extension("ProductVariation", "ComplexPriceBuyableDecorator");
-//DataObject::add_extension("ComplexPriceObject", "ComplexPriceBuyableDecorator_ComplexPriceObject");
-//MAY SET
 /**
- * ADD TO ECOMMERCE.YAML:
-ProductsAndGroupsModelAdmin:
-	managed_modules: [
-		...
-		ComplexPriceObject
-	]
-*/
-
-//===================---------------- END ecommerce_group_pricing MODULE ----------------===================
+ * author: Nicolaas - modules [at] sunnysideup.co.nz
+ */
