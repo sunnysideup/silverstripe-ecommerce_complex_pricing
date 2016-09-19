8ddcb98d75f048ee924634a35f0d215aca77ea5e
www-data <sunnysideupdevs@gmail.com>
2016-09-19 18:50:01 +1200

    fix ups

--- code/PriceListPage.php
+++ code/PriceListPage.php
@@ -11,122 +11,125 @@
  *
  **/
 
-class PriceListPage extends ProductGroup {
+class PriceListPage extends ProductGroup
+{
 
-	/**
-	 * Standard SS variable.
-	 */
-	private static $singular_name = "Price List Page";
-		function i18n_singular_name() { return _t("ProductGroup.PRICELISTPAGE", "Price List Page");}
+    /**
+     * Standard SS variable.
+     */
+    private static $singular_name = "Price List Page";
+    public function i18n_singular_name()
+    {
+        return _t("ProductGroup.PRICELISTPAGE", "Price List Page");
+    }
 
-	/**
-	 * Standard SS variable.
-	 */
-	private static $plural_name = "Price List Pages";
-		function i18n_plural_name() { return _t("ProductGroup.PRICELISTPAGES", "Price List Pages");}
+    /**
+     * Standard SS variable.
+     */
+    private static $plural_name = "Price List Pages";
+    public function i18n_plural_name()
+    {
+        return _t("ProductGroup.PRICELISTPAGES", "Price List Pages");
+    }
 
-	/**
-	 * standard SS variable
-	 * @static Array | String
-	 *
-	 */
-	private static $icon = "ecommerce_complex_pricing/images/treeicons/PriceListPage";
+    /**
+     * standard SS variable
+     * @static Array | String
+     *
+     */
+    private static $icon = "ecommerce_complex_pricing/images/treeicons/PriceListPage";
 
-	/**
-	 * standard SS variable
-	 * @static Array
-	 *
-	 */
-	private static $db = array(
-		"NumberOfLevelsToHide" => "Int"
-	);
+    /**
+     * standard SS variable
+     * @static Array
+     *
+     */
+    private static $db = array(
+        "NumberOfLevelsToHide" => "Int"
+    );
 
-	/**
-	 * standard SS variable
-	 * @static Array
-	 *
-	 */
-	private static $defaults = array(
-		"LevelOfProductsToShow" => -1,
-		"NumberOfProductsPerPage" => 100,
-		"NumberOfLevelsToHide" => 1
-	);
-
-
-	/**
-	 * standard SS variable
-	 * @static Array
-	 *
-	 */
-	private static $allowed_children = "none";
-
-
-	/**
-	 * standard SS Method
-	 * return FieldSet
-	 *
-	 */
-	function getCMSFields() {
-		$fields = parent::getCMSFields();
-		$fields->addFieldToTab('Root.ProductDisplay',new NumericField("NumberOfLevelsToHide", _t("PriceListPage.NUMBEROFLEVELSTOHIDE", "Number of levels to hide from the top down (e.g. set to one to hide main (top) product group holder). To hide all the parent product groups you can set this variable to something like 999.")));
-		return $fields;
-	}
+    /**
+     * standard SS variable
+     * @static Array
+     *
+     */
+    private static $defaults = array(
+        "LevelOfProductsToShow" => -1,
+        "NumberOfProductsPerPage" => 100,
+        "NumberOfLevelsToHide" => 1
+    );
 
 
-	/**
-	 * Retrieve a set of products, based on the given parameters.
-	 * Add Parent Group Pages to diplay within list.
-	 *
-	 * Note that you can hide the "top level"
-	 * @return DataObjectSet | Null
-	 */
-	public function currentFinalProducts($alternativeSort = null){
-		$products = parent::currentFinalProducts($alternativeSort	);
-		if($products) {
-			foreach($products as $product) {
-				$product->ParentSegments = null;
-				if($this->NumberOfLevelsToHide < 20) {
-					$segmentArray = array();
-					$item = $product;
-					while($item && $item->ParentID) {
-						$item = ProductGroup::get()->byID($item->ParentID);
-						if($item) {
-							$segmentArray[] = array(
-								"URLSegment" => $item->URLSegment,
-								"ID" => $item->ID,
-								"ClassName" => $item->ClassName,
-								"Title" => $item->Title,
-								"Link" => $item->Link()
-							);
-						}
-					}
-					if(count($segmentArray)) {
-						$product->ParentSegments = new ArrayList();
-						$segmentArray = array_reverse($segmentArray);
-						foreach($segmentArray as $key => $segment) {
-							if($key > $this->NumberOfLevelsToHide) {
-								$product->ParentSegments->push(new ArrayData($segment));
-							}
-						}
-					}
-				}
-			}
-		}
-		return $products;
-	}
+    /**
+     * standard SS variable
+     * @static Array
+     *
+     */
+    private static $allowed_children = "none";
 
+
+    /**
+     * standard SS Method
+     * return FieldSet
+     *
+     */
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        $fields->addFieldToTab('Root.ProductDisplay', new NumericField("NumberOfLevelsToHide", _t("PriceListPage.NUMBEROFLEVELSTOHIDE", "Number of levels to hide from the top down (e.g. set to one to hide main (top) product group holder). To hide all the parent product groups you can set this variable to something like 999.")));
+        return $fields;
+    }
+
+
+    /**
+     * Retrieve a set of products, based on the given parameters.
+     * Add Parent Group Pages to diplay within list.
+     *
+     * Note that you can hide the "top level"
+     * @return DataObjectSet | Null
+     */
+    public function currentFinalProducts($alternativeSort = null)
+    {
+        $products = parent::currentFinalProducts($alternativeSort);
+        if ($products) {
+            foreach ($products as $product) {
+                $product->ParentSegments = null;
+                if ($this->NumberOfLevelsToHide < 20) {
+                    $segmentArray = array();
+                    $item = $product;
+                    while ($item && $item->ParentID) {
+                        $item = ProductGroup::get()->byID($item->ParentID);
+                        if ($item) {
+                            $segmentArray[] = array(
+                                "URLSegment" => $item->URLSegment,
+                                "ID" => $item->ID,
+                                "ClassName" => $item->ClassName,
+                                "Title" => $item->Title,
+                                "Link" => $item->Link()
+                            );
+                        }
+                    }
+                    if (count($segmentArray)) {
+                        $product->ParentSegments = new ArrayList();
+                        $segmentArray = array_reverse($segmentArray);
+                        foreach ($segmentArray as $key => $segment) {
+                            if ($key > $this->NumberOfLevelsToHide) {
+                                $product->ParentSegments->push(new ArrayData($segment));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return $products;
+    }
 }
 
-class PriceListPage_Controller extends ProductGroup_Controller {
-
-
-	function init(){
-		parent::init();
-		Requirements::themedCSS("PriceListPage", "ecommerce_complex_pricing");
-	}
-
+class PriceListPage_Controller extends ProductGroup_Controller
+{
+    public function init()
+    {
+        parent::init();
+        Requirements::themedCSS("PriceListPage", "ecommerce_complex_pricing");
+    }
 }
-
-
-
-
--- code/dod/ComplexPriceBuyableDecorator.php
+++ code/dod/ComplexPriceBuyableDecorator.php
@@ -1,191 +1,199 @@
 <?php
 
 
-class ComplexPriceBuyableDecorator extends DataExtension {
+class ComplexPriceBuyableDecorator extends DataExtension
+{
+    private static $has_many = array(
+        'ComplexPriceObjects' => 'ComplexPriceObject'
+    );
 
-	private static $has_many = array(
-		'ComplexPriceObjects' => 'ComplexPriceObject'
-	);
+    public function updateCMSFields(FieldList $fields)
+    {
+        $tabName = 'Root.Pricing';
 
-	public function updateCMSFields(FieldList $fields) {
-		$tabName = 'Root.Pricing';
+        if (class_exists("DataObjectOneFieldUpdateController")) {
+            $link = DataObjectOneFieldUpdateController::popup_link(
+                $this->owner->ClassName,
+                "Price",
+                $where = "",
+                $sort = "Price ASC ",
+                $linkText = "Check all prices..."
+            );
+            $fields->AddFieldToTab($tabName, new HeaderField("MetaTitleFixesHeader", "Quick review", 3));
+            $fields->AddFieldToTab($tabName, new LiteralField("MetaTitleFixes", $link.".<br /><br /><br />"));
+        }
 
-		if(class_exists("DataObjectOneFieldUpdateController")) {
-			$link = DataObjectOneFieldUpdateController::popup_link(
-				$this->owner->ClassName,
-				"Price",
-				$where = "",
-				$sort = "Price ASC ",
-				$linkText = "Check all prices..."
-			);
-			$fields->AddFieldToTab($tabName, new HeaderField("MetaTitleFixesHeader", "Quick review", 3));
-			$fields->AddFieldToTab($tabName, new LiteralField("MetaTitleFixes", $link.".<br /><br /><br />"));
-		}
-
-		// move price field under new 'Pricing' tab
+        // move price field under new 'Pricing' tab
 //		$priceField = $fields->fieldByName('Root.Details.Price');
 //		$fields->remove($priceField);
 
-		$fields->addFieldsToTab(
-			$tabName,
-			array(
+        $fields->addFieldsToTab(
+            $tabName,
+            array(
 //				$priceField,
-				new HeaderField("ComplexPricesHeader", "Alternative Pricing", 3),
-				new LiteralField("ComplexPricesExplanation", "<p>Please enter <i>alternative</i> pricing below. You can enter a price per <a href=\"admin/security/\">security group</a> and/or per country.</p>"),
-				$this->complexPricesHasManyTable()
-			)
-		);
-	}
+                new HeaderField("ComplexPricesHeader", "Alternative Pricing", 3),
+                new LiteralField("ComplexPricesExplanation", "<p>Please enter <i>alternative</i> pricing below. You can enter a price per <a href=\"admin/security/\">security group</a> and/or per country.</p>"),
+                $this->complexPricesHasManyTable()
+            )
+        );
+    }
 
-	protected function complexPricesHasManyTable(){
-		$gridCfg = new GridFieldConfig_RelationEditor();
-		$grid = new GridField('ComplexPriceObjects', '', $this->owner->ComplexPriceObjects(), $gridCfg);
-		return $grid;
-	}
+    protected function complexPricesHasManyTable()
+    {
+        $gridCfg = new GridFieldConfig_RelationEditor();
+        $grid = new GridField('ComplexPriceObjects', '', $this->owner->ComplexPriceObjects(), $gridCfg);
+        return $grid;
+    }
 
-	function HasDiscount() {
-		if($this->owner->Price > 0) {
-			if($this->owner->Price > $this->owner->getCalculatedPrice()) {
-				return true;
-			}
-		}
-		return false;
-	}
+    public function HasDiscount()
+    {
+        if ($this->owner->Price > 0) {
+            if ($this->owner->Price > $this->owner->getCalculatedPrice()) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	function updateCalculatedPrice(&$startingPrice) {
-		$newPrice = -1;
-		$fieldName = $this->owner->ClassName."ID";
-		$singleton = ComplexPriceObject::get()->first();
-		if($singleton) {
-			// Check that ComplexPriceObject can be joined to this type of object
-			if(!$singleton->hasField($fieldName)) {
-				$ancestorArray = ClassInfo::ancestry($this->owner, true );
-				foreach($ancestorArray as $ancestor) {
-					$fieldName = $ancestor."ID";
-					if($singleton->hasField($fieldName)) {
-						break;
-					}
-				}
-			}
+    public function updateCalculatedPrice(&$startingPrice)
+    {
+        $newPrice = -1;
+        $fieldName = $this->owner->ClassName."ID";
+        $singleton = ComplexPriceObject::get()->first();
+        if ($singleton) {
+            // Check that ComplexPriceObject can be joined to this type of object
+            if (!$singleton->hasField($fieldName)) {
+                $ancestorArray = ClassInfo::ancestry($this->owner, true);
+                foreach ($ancestorArray as $ancestor) {
+                    $fieldName = $ancestor."ID";
+                    if ($singleton->hasField($fieldName)) {
+                        break;
+                    }
+                }
+            }
 
-			// Load up the alternate prices for this product
-			$prices = ComplexPriceObject::get()
-				->filter(array($fieldName => $this->owner->ID, "NoLongerValid" => 0))
-				->sort("NewPrice", "DESC");;
-			$memberGroupsArray = array();
-			if($prices->count()) {
-				// Load up the groups for the current memeber, if any
-				if($member = Member::currentUser()) {
-					if($memberGroupComponents = $member->getManyManyComponents('Groups')) {
-						if($memberGroupComponents && $memberGroupComponents->count()) {
-							$memberGroupsArray = $memberGroupComponents->column("ID");
-							if(!is_array($memberGroupsArray)) {
-								$memberGroupsArray = array();
-							}
-						}
-					}
-				}
+            // Load up the alternate prices for this product
+            $prices = ComplexPriceObject::get()
+                ->filter(array($fieldName => $this->owner->ID, "NoLongerValid" => 0))
+                ->sort("NewPrice", "DESC");
+            ;
+            $memberGroupsArray = array();
+            if ($prices->count()) {
+                // Load up the groups for the current memeber, if any
+                if ($member = Member::currentUser()) {
+                    if ($memberGroupComponents = $member->getManyManyComponents('Groups')) {
+                        if ($memberGroupComponents && $memberGroupComponents->count()) {
+                            $memberGroupsArray = $memberGroupComponents->column("ID");
+                            if (!is_array($memberGroupsArray)) {
+                                $memberGroupsArray = array();
+                            }
+                        }
+                    }
+                }
 
-				$countryID = EcommerceCountry::get_country_id();
+                $countryID = EcommerceCountry::get_country_id();
 
-				// Look at each price and see if it can be used
-				foreach($prices as $price) {
-					$priceCanBeUsed = true;
+                // Look at each price and see if it can be used
+                foreach ($prices as $price) {
+                    $priceCanBeUsed = true;
 
-					// Does it pass the group filter?
-					if($priceGroupComponents = $price->getManyManyComponents('Groups')) {
-						if($priceGroupComponents && $priceGroupComponents->count()) {
-							$priceCanBeUsed = false;
-							$priceGroupArray = $priceGroupComponents->column("ID");
-							if(!is_array($priceGroupArray)) {$priceGroupArray = array();}
-							$interSectionArray = array_intersect($priceGroupArray, $memberGroupsArray);
-							if(is_array($interSectionArray) && count($interSectionArray)) {
-								$priceCanBeUsed = true;
-							}
-						}
-					}
+                    // Does it pass the group filter?
+                    if ($priceGroupComponents = $price->getManyManyComponents('Groups')) {
+                        if ($priceGroupComponents && $priceGroupComponents->count()) {
+                            $priceCanBeUsed = false;
+                            $priceGroupArray = $priceGroupComponents->column("ID");
+                            if (!is_array($priceGroupArray)) {
+                                $priceGroupArray = array();
+                            }
+                            $interSectionArray = array_intersect($priceGroupArray, $memberGroupsArray);
+                            if (is_array($interSectionArray) && count($interSectionArray)) {
+                                $priceCanBeUsed = true;
+                            }
+                        }
+                    }
 
-					// Does it pass the country filter?
-					if($priceCanBeUsed) {
-						if($priceCountryComponents = $price->getManyManyComponents('EcommerceCountries')) {
-							if($priceCountryComponents && $priceCountryComponents->count()) {
-								$priceCanBeUsed = false;
-								$priceCountryArray = $priceCountryComponents->column("ID");
-								if(!is_array($priceCountryArray)) {$priceCountryArray = array();}
-								if($countryID && in_array($countryID, $priceCountryArray)) {
-									$priceCanBeUsed = true;
-								}
-							}
-						}
-					}
+                    // Does it pass the country filter?
+                    if ($priceCanBeUsed) {
+                        if ($priceCountryComponents = $price->getManyManyComponents('EcommerceCountries')) {
+                            if ($priceCountryComponents && $priceCountryComponents->count()) {
+                                $priceCanBeUsed = false;
+                                $priceCountryArray = $priceCountryComponents->column("ID");
+                                if (!is_array($priceCountryArray)) {
+                                    $priceCountryArray = array();
+                                }
+                                if ($countryID && in_array($countryID, $priceCountryArray)) {
+                                    $priceCanBeUsed = true;
+                                }
+                            }
+                        }
+                    }
 
-					// Does it pass the date filter?
-					if($priceCanBeUsed) {
-						$nowTS = strtotime("now");
-						if($price->From) {
-							$priceCanBeUsed = false;
-							$fromTS = strtotime($price->From);
-							if($fromTS && $fromTS < $nowTS) {
-								$priceCanBeUsed = true;
-							}
-						}
-					}
+                    // Does it pass the date filter?
+                    if ($priceCanBeUsed) {
+                        $nowTS = strtotime("now");
+                        if ($price->From) {
+                            $priceCanBeUsed = false;
+                            $fromTS = strtotime($price->From);
+                            if ($fromTS && $fromTS < $nowTS) {
+                                $priceCanBeUsed = true;
+                            }
+                        }
+                    }
 
-					if($priceCanBeUsed) {
-						if($price->Until) {
-							$priceCanBeUsed = false;
-							$untilTS = strtotime($price->Until);
-							if($untilTS && $untilTS > $nowTS) {
-								$priceCanBeUsed = true;
-							}
-						}
-					}
+                    if ($priceCanBeUsed) {
+                        if ($price->Until) {
+                            $priceCanBeUsed = false;
+                            $untilTS = strtotime($price->Until);
+                            if ($untilTS && $untilTS > $nowTS) {
+                                $priceCanBeUsed = true;
+                            }
+                        }
+                    }
 
-					// If so, apply the price
-					if($priceCanBeUsed) {
-						$newPrice = $price->getCalculatedPrice();
-					}
-				}
-			}
-		}
+                    // If so, apply the price
+                    if ($priceCanBeUsed) {
+                        $newPrice = $price->getCalculatedPrice();
+                    }
+                }
+            }
+        }
 
-		if($newPrice > -1) {
-			$startingPrice = $newPrice;
-		}
+        if ($newPrice > -1) {
+            $startingPrice = $newPrice;
+        }
 
-		return $startingPrice;
-	}
-
+        return $startingPrice;
+    }
 }
 
 
-class ComplexPriceBuyableDecorator_ComplexPriceObject extends DataExtension {
+class ComplexPriceBuyableDecorator_ComplexPriceObject extends DataExtension
+{
+    public static function get_extra_config($class, $extension, $args)
+    {
+        $buyables = EcommerceConfig::get("EcommerceDBConfig", "array_of_buyables");
+        $hasOneArray = array();
+        if ($buyables && is_array($buyables) && count($buyables)) {
+            foreach ($buyables as $item) {
+                $hasOneArray[$item] = $item;
+            }
+            return array(
+                'has_one' => $hasOneArray
+            );
+        }
+        return array();
+    }
 
-	public static function get_extra_config($class, $extension, $args) {
-		$buyables = EcommerceConfig::get("EcommerceDBConfig", "array_of_buyables");
-		$hasOneArray = array();
-		if($buyables && is_array($buyables) && count($buyables)) {
-			foreach($buyables as $item) {
-				$hasOneArray[$item] = $item;
-			}
-			return array (
-				'has_one' => $hasOneArray
-			);
-		}
-		return array();
-	}
-
-	public function updateCMSFields(FieldList $fields) {
-		$this->owner->getBuyable();
-		$buyables = EcommerceConfig::get("EcommerceDBConfig", "array_of_buyables");
-		if($buyables && is_array($buyables) && count($buyables)) {
-			foreach($buyables as $item) {
-				$fields->replaceField($item."ID", new HiddenField($item."ID"));
-			}
-		}
-		$fields->replaceField("From", new TextField("From"));
-		return $fields;
-	}
-
-
+    public function updateCMSFields(FieldList $fields)
+    {
+        $this->owner->getBuyable();
+        $buyables = EcommerceConfig::get("EcommerceDBConfig", "array_of_buyables");
+        if ($buyables && is_array($buyables) && count($buyables)) {
+            foreach ($buyables as $item) {
+                $fields->replaceField($item."ID", new HiddenField($item."ID"));
+            }
+        }
+        $fields->replaceField("From", new TextField("From"));
+        return $fields;
+    }
 }
